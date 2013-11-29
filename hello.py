f7155ffc29c304fe6feaca4059766ec3894f7b83
Ben Fulton <ben@benfulton.net>
2013-11-28 23:49:03 -0500

    Add CSS and a search route

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+	return render_template('index.html')
 
+@app.route('/search/<term>')
+def profile(term):
+	return "No results for " + term
+
+if __name__ == '__main__':
+    app.run(debug=True)
