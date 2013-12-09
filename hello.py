2ae6138ba701ea0fce49a622848285b489dbbc57
Ben Fulton <ben@benfulton.net>
2013-12-08 21:17:57 -0500

    Add favicon and a few other changes

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 db = None
+article_db = None
 pref = """PREFIX prot:   <http://purl.uniprot.org/core/>
           PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
           PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -16,6 +17,13 @@ def load_db():
         db = rdflib.Graph()
         db.parse("myopathy.rdf", format="xml")
     return db
+
+def load_articles():
+    global article_db
+    if article_db == None:
+        article_db = rdflib.Graph()
+        article_db.parse("citations-myopathy-uniprot.rdf", format="xml")
+    return article_db
 
 @app.route('/')
 def hello():
@@ -41,26 +49,41 @@ def protein(mnemonic):
     print "Result count",len(r)
     return render_template('protein.html', results=r, mnemonic=mnemonic)
 
-@app.route('/articles')
-def articles():
-    db = rdflib.Graph()
-    db.parse("articles.rdf", format="xml")
-    q = """select ?r ?d where { ?d <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.uniprot.org/core/Journal_Citation>
-        . ?d <http://purl.uniprot.org/core/title> ?r}"""
-    r = db.query(q)
-    return render_template('articles.html', results=r)
+@app.route('/articles/page/<page>')
+def articles(page):
+    db = load_articles()
+    q = """select ?title ?cite where
+            { ?cite rdf:type prot:Journal_Citation;
+              prot:title ?title }
+           order by ?title
+           limit 20
+           offset %s
+            """
+    print pref + q
+    r = db.query(pref + q % ((int(page)-1) * 20))
+    return render_template('articles.html', results=r, page=int(page))
+
+@app.route('/articles/author/<author>')
+def articles_by_author(author):
+    db = load_articles()
+    q = """select ?title ?cite where
+            { ?cite rdf:type prot:Journal_Citation;
+              prot:title ?title;
+              prot:author "%s" }
+           order by ?title
+            """
+    print pref + q
+    r = db.query(pref + q % author)
+    return render_template('articles.html', results=r, page=1)
 
 @app.route('/article/<pmid>')
 def article(pmid):
-    db = rdflib.Graph()
-    db.parse("articles.rdf", format="xml")
+    db = load_articles()
     q = """select ?d ?author ?comment where
-            { cite:%s rdf:type
-              prot:Journal_Citation;
+            { cite:%s rdf:type prot:Journal_Citation;
               prot:title ?d;
               prot:author ?author;
               rdfs:comment ?comment }"""
-    print pref + q % pmid
     r = db.query(pref + q % pmid)
     d = dict(title = list(r)[0][0], authors = [f[1] for f in r], pmid=pmid, comment=list(r)[0][2])
     return render_template('article.html', article= d)
